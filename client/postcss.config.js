0431c0bcaf0439b7e9d9b1128c15286281c1c985
Daan153 <creative.in47@gmail.com>
2025-12-04 07:53:14 +0530

    Initial project created client

---
+++ client/postcss.config.js
@@ -0,0 +1,8 @@
+// U:\fullstack-blog\client\postcss.config.js
+module.exports = {
+  plugins: {
+    // These need to be objects or strings specifying the plugin
+    'tailwindcss': {},
+    'autoprefixer': {},
+  },
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this based on your file structure
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
